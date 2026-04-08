b1ad3455e043603d75b0e1072def1d1ecb6e5913
danishdynamic <shakeali.ali@gmail.com>
2026-04-08 13:09:35 +0530

    feat(db): establish connection with mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,19 @@
+
+import 'dotenv/config'; // This loads variables IMMEDIATELY before other imports run
+
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js'; 
-
-// if the "type": "module" is set in package.json, you can use ES6 import syntax or we can use const express = require('express'); if we are using CommonJS syntax.
+import connectDB from './config/db.js';
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
+// Now this will have access to the URI
+connectDB(); 
 
-app.use ("/api/notes", notesRoutes); // Importing the notes routes and using them with the base path /api/notes
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use("/api/notes", notesRoutes);
 
-
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
- 
-/* app.get('/api/notes', (req, res) => {
-    res.status(200).send('Creating Note...!');  / moving them to router so that we can keep our server.js file clean and organized, and we can easily manage our routes in separate files.
-});
-
-app.post('/api/notes', (req, res) => {
-    res.status(201).json({'message': 'Note created successfully!'});
-});
-
-app.put('/api/notes/:id', (req, res) => {
-    res.status(200).json({'message': 'Note updated successfully!'});
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    res.status(200).json({'message': 'Note deleted successfully!'});
-}); */
