376b8fb91d5d2373d45cf89356e74e466f5ed963
danishdynamic <shakeali.ali@gmail.com>
2026-04-17 00:52:23 +0530

    fix : rate limiter as per notes entered and enbaling CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,18 @@
 
 import 'dotenv/config'; // This loads variables IMMEDIATELY before other imports run
-
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js'; 
 import connectDB from './config/db.js';
-import rateLimiter from './middleware/rateLimiter.js'; // Import the rate limiter middleware
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
+// enable CORS so that our frontend can communicate with the backend without issues. In production, you might want to configure CORS more restrictively to only allow requests from your frontend domain for better security.
+app.use(cors());
 
 app.use(express.json()); 
-// Global Middleware to parse JSON bodies and runs before each route handler i.e app.use() 
-// it doesnt directly interacts with routes and controllers.
-// express.json() runs first and parses body i.e attaches to req.body inside notesController.js and then the route handler runs and can access req.body.
-// app.use(rateLimiter); we can apply ratelimiter globally or better inside routes that modify data (POST, PUT, DELETE) to prevent abuse. Applying globally would also limit GET requests which might not be necessary. So we will apply it inside notesRoutes.js for POST, PUT, DELETE routes.
-// our custom middleware to log incoming requests (for debugging purposes)
-//app.use((req, res, next) => {
-  //console.log (`✅ Request method is ${req.method}, req.url is ${req.url}`);
-  //next();
-//});
+
 
 app.use("/api/notes", notesRoutes); // This means that for any request that starts with /api/notes, the notesRoutes will handle it.
 
@@ -30,3 +22,14 @@ connectDB().then(() => {
     console.log(`Server is running on port ${PORT}`)
   });
 });
+
+
+// Global Middleware to parse JSON bodies and runs before each route handler i.e app.use() 
+// it doesnt directly interacts with routes and controllers.
+// express.json() runs first and parses body i.e attaches to req.body inside notesController.js and then the route handler runs and can access req.body.
+// app.use(rateLimiter); we can apply ratelimiter globally or better inside routes that modify data (POST, PUT, DELETE) to prevent abuse. Applying globally would also limit GET requests which might not be necessary. So we will apply it inside notesRoutes.js for POST, PUT, DELETE routes.
+// our custom middleware to log incoming requests (for debugging purposes)
+//app.use((req, res, next) => {
+  //console.log (`✅ Request method is ${req.method}, req.url is ${req.url}`);
+  //next();
+//});
